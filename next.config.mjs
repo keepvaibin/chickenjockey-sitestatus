cc2ec926847be99e640f8f4c9ef054bc540f8e4b
Vaibhav Shukla <keep.vaibin@gmail.com>
2025-12-16 16:18:00 -0800

    out page

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: "export",
+  images: { unoptimized: true },
+  basePath: "/chickenjockey-sitestatus",
+  assetPrefix: "/chickenjockey-sitestatus/",
 };
 
 export default nextConfig;
