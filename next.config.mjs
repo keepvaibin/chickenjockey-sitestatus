0a9546ca29693aa5ec268c2cdbd4b103eb0d0c71
Vaibhav Shukla <keep.vaibin@gmail.com>
2025-12-16 16:33:23 -0800

    no jekyll

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const nextConfig = {
   output: "export",
   images: { unoptimized: true },
   basePath: "/chickenjockey-sitestatus",
-  assetPrefix: "/chickenjockey-sitestatus/",
 };
 
 export default nextConfig;
